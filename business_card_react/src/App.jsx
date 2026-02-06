285bb204de740dc1097edc61e35c33aeb95be2a7
lauramj2 <lauraamarie2@gmail.com>
2026-02-06 14:53:35 -0500

    fixed project setup

--- business_card_react/src/App.jsx
+++ business_card_react/src/App.jsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import About from "./Components/About.jsx"
+import Footer from "./Components/Footer.jsx"
+import Info from "./Components/Info.jsx"
+import Interests from "./Components/Interests.jsx"
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div id="card">
+        <Info />
+        <About />
+        <Interests  header ="Interests" hobbies="Reading, playing video games, hiking" />
+        <Footer />
+    </div>
   )
 }
 
-export default App
+
